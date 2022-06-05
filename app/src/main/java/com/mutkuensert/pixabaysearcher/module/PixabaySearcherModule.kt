55b81ce2f5a64ee353fa9df49a79b782b97be9d3
Mustafa Utku Ensert <mutkuensert@hotmail.com>
2022-06-05 15:03:42 +0300

    PixabaySearcherModule has been created.

---
+++ app/src/main/java/com/mutkuensert/pixabaysearcher/module/PixabaySearcherModule.kt
@@ -0,0 +1,27 @@
+package com.mutkuensert.pixabaysearcher.module
+
+import com.mutkuensert.pixabaysearcher.service.PixabayService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PixabaySearcherModule {
+
+    @Singleton
+    @Provides
+    fun providePixabayService(): PixabayService {
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+        return Retrofit.Builder().baseUrl("https://pixabay.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build().create(PixabayService::class.java)
+    }
+}
--- app/src/main/java/com/mutkuensert/pixabaysearcher/view/MainScreenFragment.kt
+++ app/src/main/java/com/mutkuensert/pixabaysearcher/view/MainScreenFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mutkuensert.pixabaysearcher.databinding.FragmentMainScreenBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainScreenFragment : Fragment() {
     private lateinit var binding: FragmentMainScreenBinding
     override fun onCreateView(
